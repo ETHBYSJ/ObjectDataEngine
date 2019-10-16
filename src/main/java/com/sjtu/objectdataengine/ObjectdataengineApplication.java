59e554e109f2ec61ad0f0bd15f02ab2ee8ecb0b1
ShuiShuiMeng <981904597@qq.com>
2019-10-16 16:02:15 +0800

    123

--- src/main/java/com/sjtu/objectdataengine/ObjectdataengineApplication.java
+++ src/main/java/com/sjtu/objectdataengine/ObjectdataengineApplication.java
@@ -1,17 +1,13 @@
 package com.sjtu.objectdataengine;
 
-import com.sjtu.objectdataengine.model.ObjectTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
-import java.util.HashSet;
-
 @SpringBootApplication
-public class ObjectDataEngineApplication {
+public class ObjectdataengineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ObjectDataEngineApplication.class, args);
+        SpringApplication.run(ObjectdataengineApplication.class, args);
         /*HashSet<String> attr = new HashSet<String>();
         ObjectTemplate objectTemplate = new ObjectTemplate("1", "123", attr);
         objectTemplate.setCreateTime(new Date());*/
